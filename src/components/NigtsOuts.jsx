287eb1519a2e4ce5e68a21e18ab5d427f85f72ef
Tijs Martens <tijs@bothrs.com>
2020-10-12 21:16:29 +0200

    get (debatetable time to bed)

--- src/components/NigtsOuts.jsx
+++ src/components/NigtsOuts.jsx
@@ -9,12 +9,27 @@ import {} from "react";
 function NightsOut() {
   const { userData } = useContext(WithingsContext);
   const [averageSleepHr, setAverageSleepHr] = useState(0);
-  console.log("sleepdata", userData.sleep);
+  const [averageSleepTimeInMinutes, setAverageSleepTimeInMinutes] = useState(0);
 
   useEffect(() => {
     if (userData.sleep) {
       setAverageSleepHr(
         getAverage(userData.sleep.map((night) => night.data.hr_average))
+      );
+
+      setAverageSleepTimeInMinutes(
+        getAverage(
+          userData.sleep.map((night) => {
+            const date = new Date(night.startdate * 1000);
+            let minutesInDay = date.getHours() * 60 + date.getMinutes();
+            //you can't go to sleep after 10 in the morning
+            if (minutesInDay < 60 * 10) {
+              minutesInDay = minutesInDay =
+                (date.getHours() + 24) * 60 + date.getMinutes();
+            }
+            return minutesInDay;
+          })
+        )
       );
     }
   }, [userData]);
@@ -27,6 +42,9 @@ function NightsOut() {
         can guess if you went out or not
       </SubTitle>
       your average HR during the nights is {Math.round(averageSleepHr)}
+      <br />
+      your average time to go to bed is
+      {((averageSleepTimeInMinutes / 60)%24).toFixed(2).toString().replace(".", ":")}
     </Section>
   );
 }
