6963abaecd2d1db8458eb734dc36eddd6a4f62e0
Tijs Martens <tijs@bothrs.com>
2020-10-17 10:18:57 +0200

    get average night duration

--- src/components/NigtsOuts.jsx
+++ src/components/NigtsOuts.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { WithingsContext } from "../contexts/WithingsContext";
 import getAverage from "../lib/getAverage";
-import getLastDays from "../lib/getLastDays";
 
 import { Section } from "../styles/Section";
 import { H2, SubTitle } from "../styles/Types";
@@ -9,8 +8,18 @@ import {} from "react";
 
 function NightsOut() {
   const { userData } = useContext(WithingsContext);
+
+  // in bpm
   const [averageSleepHr, setAverageSleepHr] = useState(0);
-  const [averageSleepTimeInMinutes, setAverageSleepTimeInMinutes] = useState(0);
+  const [barrierSleepHr, setBarrierSleepHr] = useState(0);
+
+  // in minutes from start of the day
+  const [averageTimeToBed, setAverageTimeToBed] = useState(0);
+  const [barrierTimeToBed, setBarrierTimeToBed] = useState(0);
+
+  // in minutes
+  const [averateNightDuration, setAverageNightDuration] = useState(0);
+  const [barrierNightDuration, setBarrierNightDuration] = useState(0);
 
   useEffect(() => {
     if (userData.sleep) {
@@ -18,7 +27,7 @@ function NightsOut() {
         getAverage(userData.sleep.map((night) => night.data.hr_average))
       );
 
-      setAverageSleepTimeInMinutes(
+      setAverageTimeToBed(
         getAverage(
           userData.sleep.map((night) => {
             const date = new Date(night.startdate * 1000);
@@ -32,14 +41,43 @@ function NightsOut() {
           })
         )
       );
+
+      setAverageNightDuration(
+        getAverage(
+          userData.sleep.map((night) => {
+            const startNight = new Date(night.startdate * 1000);
+            const endNight = new Date(night.enddate * 1000);
+            const nightDuration = endNight - startNight; // duration in miliseconds
+            return nightDuration / (1000 * 60); // duration in minutes
+          })
+        )
+      );
     }
   }, [userData]);
+
+  useEffect(() => {
+    if (averageSleepHr !== 0) {
+      setBarrierSleepHr(averageSleepHr * 1.08);
+    }
+  }, [averageSleepHr]);
+
+  useEffect(() => {
+    if (averageTimeToBed !== 0) {
+      setBarrierTimeToBed(averageTimeToBed + 120);
+    }
+  }, [averageTimeToBed]);
+
+  useEffect(() => {
+    setBarrierNightDuration(averateNightDuration + 90)
+  }, [averateNightDuration]);
 
   const formatTimeFromMinutes = (minutes) => {
     const hr = Math.floor((minutes / 60) % 24);
     const min = Math.round(((((minutes / 60) % 24) - hr) / 100) * 60 * 100);
     return `${hr}:${min < 10 ? `0${min}` : min}`;
   };
+
+  console.log(userData.sleep && userData.sleep[0]);
 
   return (
     <Section>
@@ -48,10 +86,20 @@ function NightsOut() {
         By looking at your average heart raten and the time you went to bed we
         can guess if you went out or not
       </SubTitle>
-      your average HR during the nights is {Math.round(averageSleepHr)}
+      your average HR during the nights is {Math.round(averageSleepHr)}bpm
       <br />
       your average time to go to bed is{" "}
-      {formatTimeFromMinutes(averageSleepTimeInMinutes)}
+      {formatTimeFromMinutes(averageTimeToBed)}
+      <br />
+      your average night duration is {formatTimeFromMinutes(averateNightDuration)}
+      <hr />
+      <hr />
+      you are concidered drunk when your average hr is bigger or equal then{" "}
+      {Math.round(barrierSleepHr)}bpm
+      <br />
+      you went out if you fell asleep at:
+      {formatTimeFromMinutes(barrierTimeToBed)}
+      you didn't sleep enough when you slept: {formatTimeFromMinutes(barrierNightDuration)}
     </Section>
   );
 }
