51bc68b1d22491ec1b8ad7b67a5dc93730b16ade
Tijs Martens <tijs@bothrs.com>
2020-10-22 07:24:35 +0200

    extract router from app.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { WithingsProvider } from "./contexts/WithingsContext";
 
 import Auth from "./pages/Auth";
-import Feed from "./pages/Feed";
+import Router from "./Router";
 
-import useAuth from './hooks/useAuth'
-
-import HeartRate from "./pages/detail/HeartRate";
-import NightDuration from "./pages/detail/SleepDuration";
-import Calories from "./pages/detail/Calories";
-import DeepSleep from "./pages/detail/DeepSleep";
-import TimeToBed from "./pages/detail/TimeToBed"
+import useAuth from "./hooks/useAuth";
 
 import Theme from "./Theme";
 
 function App() {
-  const isAuthenticated = useAuth()
+  const isAuthenticated = useAuth();
 
   return (
     <Theme>
       {isAuthenticated ? (
         <WithingsProvider>
-          <Router>
-            <Switch>
-              <Route path="/heart-rate">
-                <HeartRate />
-              </Route>
-              <Route path="/night-duration">
-                <NightDuration />
-              </Route>
-              <Route path="/burned-calories">
-                <Calories />
-              </Route>
-              <Route path="/deep-sleep">
-                <DeepSleep />
-              </Route>
-              <Route path="/time-to-bed">
-                <TimeToBed />
-              </Route>
-              <Route path="/">
-                <Feed />
-              </Route>
-            </Switch>
-          </Router>
+          <Router />
         </WithingsProvider>
       ) : (
         <Auth />
---
+++ src/Router.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Feed from "./pages/Feed";
+import HeartRate from "./pages/detail/HeartRate";
+import NightDuration from "./pages/detail/SleepDuration";
+import Calories from "./pages/detail/Calories";
+import DeepSleep from "./pages/detail/DeepSleep";
+import TimeToBed from "./pages/detail/TimeToBed"
+
+function Routing() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/heart-rate">
+          <HeartRate />
+        </Route>
+        <Route path="/night-duration">
+          <NightDuration />
+        </Route>
+        <Route path="/burned-calories">
+          <Calories />
+        </Route>
+        <Route path="/deep-sleep">
+          <DeepSleep />
+        </Route>
+        <Route path="/time-to-bed">
+          <TimeToBed />
+        </Route>
+        <Route path="/">
+          <Feed />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default Routing;
