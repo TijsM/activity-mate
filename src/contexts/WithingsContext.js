3bf394e730fe456e113ffc858902ac3f1f35c7c9
Tijs Martens <tijs@bothrs.com>
2020-10-22 21:55:14 +0200

    add Workouts card

--- src/contexts/WithingsContext.js
+++ src/contexts/WithingsContext.js
@@ -1,26 +1,32 @@
 import React, { createContext, useState, useEffect } from "react";
 import { getActivities, getSleep, getWorkouts } from "../lib/fetchWithings";
 
+import filterWorkouts from "../lib/filterWorkouts";
+
 export const WithingsContext = createContext(null);
 
-
-export const WithingsProvider = ({children, isAuthenticated}) => {
-    const [userData, setUserData] = useState({})
+export const WithingsProvider = ({ children, isAuthenticated }) => {
+  const [userData, setUserData] = useState({});
 
   useEffect(() => {
     const fetchData = async () => {
       const activities = await getActivities();
       const sleep = await getSleep();
-      const workouts = await getWorkouts()
-      setUserData({ dailyData: activities, sleep, workouts });
+      const workouts = await getWorkouts();
+      const filteredWorkouts = filterWorkouts(workouts);
+      setUserData({
+        dailyData: activities,
+        sleep,
+        workouts: filteredWorkouts,
+      });
     };
 
     fetchData();
   }, [isAuthenticated]);
 
-
-  return  <WithingsContext.Provider value={{ userData, setUserData }}>
+  return (
+    <WithingsContext.Provider value={{ userData, setUserData }}>
       {children}
-  </WithingsContext.Provider>
-
-}
+    </WithingsContext.Provider>
+  );
+};
