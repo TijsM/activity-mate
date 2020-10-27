64056a090cf51a9e1f19647277342f36d250cfaf
Tijs Martens <tijs@bothrs.com>
2020-10-27 07:23:46 +0100

    add splashscreen logic

---
+++ src/components/SplashScreen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SplashScreen() {
+  return <div>splashing</div>;
+}
+
+export default SplashScreen
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import styled from "styled-components";
+import { WithingsContext } from "../contexts/WithingsContext";
 
 import LogoHeader from "../components/LogoHeader";
 import Content from "../components/feed/Content";
+import SplashScreen from "../components/SplashScreen";
 
 const Container = styled.section`
   display: flex;
@@ -10,11 +12,23 @@ const Container = styled.section`
 `;
 
 function Feed() {
-  return (
+  const { userData } = useContext(WithingsContext);
+
+  const [dataIsLoaded, setDataIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(userData).length > 1) {
+      setDataIsLoaded(true);
+    }
+  }, [userData]);
+
+  return dataIsLoaded ? (
     <Container>
       <LogoHeader />
       <Content />
     </Container>
+  ) : (
+    <SplashScreen />
   );
 }
 
